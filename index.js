22952e45769228d6f9f26ded00635c5213fbd059
ChloeSAPage <chloe.s.a.page@Gmail.com>
2023-12-24 21:57:15 +0000

    added a random button to choose a random colour

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-function paint(color) {
+function paint(colour) {
     const circle = document.getElementById('circleID');
-    circle.style = `background-color:${color}`;
+    circle.style = `background-color:${colour}`;
+
+    if (colour === "random") {
+        let n = (Math.random() * 0xfffff * 1000000).toString(16);
+        let hex = n.slice(0, 6)
+        circle.style = `background-color:#${hex}`;
+    }
 }
